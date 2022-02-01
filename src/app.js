bfb7b660646f6e23ba0e08dbedb5503243de29a5
Joel Santiago <jsant288@fiu.edu>
2022-02-01 02:58:00 +0000

    created random card generator

--- src/app.js
+++ src/app.js
@@ -4,8 +4,11 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+let numGen = Math.floor(Math.random() * 13);
+document.getElementById("number").innerHTML = numGen;
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+var suitArray = ["♦", "♥", "♠ ", "♣"];
+let suitGen = suitArray[Math.floor(Math.random() * suitArray.length)];
+
+document.getElementById("suitTop").innerHTML = suitGen;
+document.getElementById("suitBottom").innerHTML = suitGen;
